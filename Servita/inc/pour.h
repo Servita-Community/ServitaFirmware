497ebe012001ea8b04504aed4ac2fb9a324360bd
TeaBear5 <tyler.bair@protonmail.com>
2024-05-28 22:04:27 -0400

    Adds intial files for pour system

---
+++ Servita/inc/pour.h
@@ -0,0 +1,16 @@
+/**
+ * @file pour.h
+ * @author Tyler Bair (tyler.bair@protonmail.com)
+ * @brief Pour size management, sequencing, and control.
+ * @version 0.1
+ * @date 2024-05-28
+*/
+
+#ifndef POUR_H
+#define POUR_H
+
+
+
+
+
+#endif // POUR_H
---
+++ Servita/pour.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file pour.cpp
+ * @author Tyler Bair (tyler.bair@protonmail.com)
+ * @brief Pour size management, sequencing, and control.
+ * @version 0.1
+ * @date 2024-05-28
+*/
+#include "inc/pour.h"
