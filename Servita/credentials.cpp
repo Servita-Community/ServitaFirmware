d8324dab66bba9f23ac132b4e7d2b7abe3369a4a
TeaBear5 <tyler.bair@protonmail.com>
2024-06-04 02:04:38 -0400

    Adds initial flie structure for the credntials files

---
+++ Servita/credentials.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file credentials.cpp
+ * @brief This file contains the credentials for the WiFi network, as well as the captive portal functions.
+ * @version 0.1
+ * @date 2024-06-04
+*/
+
+#include "inc/credentials.h"
---
+++ Servita/inc/credentials.h
@@ -0,0 +1,16 @@
+/**
+ * @file credentials.h
+ * @brief This file contains the credentials for the WiFi network, as well as the captive portal functions.
+ * @version 0.1
+ * @date 2024-06-04
+*/
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+
+
+
+#endif  // CREDENTIALS_H
